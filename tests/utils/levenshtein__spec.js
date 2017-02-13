d0ab136230bb25b5cb100f101c38d09d1604cb07
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-13 21:49:00 -0200

    Mais testes, testando funcao levenshtein

---
+++ tests/utils/levenshtein__spec.js
@@ -0,0 +1,27 @@
+var levenshtein = require('./../../lib/utils/levenshtein');
+
+describe("Testa função levenshtein", function() {
+  it("Testa inserção", function() {
+    var word = 'olx';
+    var word2 = 'olxz';
+    expect(levenshtein(word, word2)).toEqual(1);
+  });
+
+  it("Testa deleção", function() {
+    var word = 'olx';
+    var word2 = 'ol';
+    expect(levenshtein(word, word2)).toEqual(1);
+  });
+
+  it("Testa substituição", function() {
+    var word = 'olx';
+    var word2 = 'olz';
+    expect(levenshtein(word, word2)).toEqual(1);
+  });
+
+  it("Testa 3 edições", function() {
+    var word = 'olxbr';
+    var word2 = 'olxus_';
+    expect(levenshtein(word, word2)).toEqual(3);
+  });
+});
