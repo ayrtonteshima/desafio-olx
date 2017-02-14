2c38fba28dfdebe38bd44637a7cbf674fab26468
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-14 10:01:46 -0200

    Adicionando log para o hapi

--- src/server.js
+++ src/server.js
@@ -1,14 +1,42 @@
 import Hapi from 'hapi';
+import Good from 'good';
 import searchRoutes from './routes/search';
 import * as connectionConfigs from './configs/connection';
 
 const server = new Hapi.Server();
-server.connection({ port: connectionConfigs.PORT, host: connectionConfigs.HOST });
+
+server.connection({
+  port: connectionConfigs.PORT,
+  host: connectionConfigs.HOST
+});
 
 server.route(searchRoutes);
 
-server.start((err) => {
-  if (err) throw err;
+// Registrando log
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}, (err) => {
+  if (err) {
+    throw err;
+  }
 
-  console.log(`Servidor rodando em: ${server.info.uri}`);
+  server.start((serverErr) => {
+    if (serverErr) throw serverErr;
+
+    console.log(`Servidor rodando em: ${server.info.uri}`);
+  });
 });
