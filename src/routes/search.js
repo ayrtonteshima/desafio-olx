afe6c5ff9f0d06e81b053f28d75e0737b220d706
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-14 10:09:41 -0200

    Definindo rotas no hapijs

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,1 +1,23 @@
-export default [];
+export default [
+  {
+    method: 'GET',
+    path: '/api/v1/words',
+    handler: function(request, reply) {
+      reply(['words']);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/v1/words/{word}',
+    handler: function(request, reply) {
+      reply([request.params.word]);
+    }
+  },
+  {
+    method: 'POST',
+    path: '/api/v1/words/{word}',
+    handler: function(request, reply) {
+      reply([request.params.word]);
+    }
+  }
+];
