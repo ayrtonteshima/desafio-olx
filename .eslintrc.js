05f60f0d2454c3de30fcc800e343d0c686ff9613
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-15 11:20:17 -0200

    Desligando configuração do eslint que invalida __ por causa do ramda

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "extends": "airbnb-base",
   "rules": {
     "comma-dangle": "off",
-    "eol-last": "off"
+    "eol-last": "off",
+    "no-underscore-dangle": "off"
   },
   "plugins": [
     "import"
