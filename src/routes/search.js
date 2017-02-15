3e464c2e115c6a082fc1f679563c97754cf7a5aa
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-15 07:22:08 -0200

    Rota das palavras filtradas com threshold

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,23 +1,25 @@
+import Word from './../models/word';
+import * as searchWordsHandler from './../handlers/search';
+
 export default [
   {
     method: 'GET',
     path: '/api/v1/words',
-    handler: function(request, reply) {
-      reply(['words']);
-    }
+    handler: searchWordsHandler.getAllWords
   },
   {
     method: 'GET',
     path: '/api/v1/words/{word}',
-    handler: function(request, reply) {
-      reply([request.params.word]);
-    }
+    handler: searchWordsHandler.getWordsFiltered
+  },
+  {
+    method: 'GET',
+    path: '/api/v1/words/{word}/{threshold}',
+    handler: searchWordsHandler.getWordsFiltered
   },
   {
     method: 'POST',
     path: '/api/v1/words/{word}',
-    handler: function(request, reply) {
-      reply([request.params.word]);
-    }
+    handler: searchWordsHandler.storeWord
   }
 ];
