7afd06923d1b1d015a416d375b461b260376a922
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-16 12:53:38 -0200

    removendo comentário do arquivo levenshtein.js

--- src/utils/levenshtein.js
+++ src/utils/levenshtein.js
@@ -7,16 +7,6 @@ const getMatrixBase = R.useWith(R.prepend, [
   R.compose(R.aperture(1), R.range(1), rangeToIndex)
 ]);
 
-/*
-** A principal ideia do projeto era fazer todo em Ramda e refatorar
-** todo algoritmo abaixo para tal, usando os conceitos de estilo
-** funcional como pure function e composition. A parte de baixo é
-** mais complexa e acabou não dando tempo de refatorar completamente.
-** Foi um ótimo desafio pensar em funcional e ramda com o algoritmo
-** levenshtein, com toda certeza vou refatorar após o desafio :)
-**
-*/
-
 const getPrevValue = (matrix, i, j) => matrix[i - 1][j - 1];
 
 const getPenality = (matrix, i, j) => (Math.min.apply(null, [
